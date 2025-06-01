9e0e22f764047dc59281cfc0cb75a5233aac73c7
Tanmay Vaij <tanmayvaij22@gmail.com>
2025-06-01 20:32:03 +0530

    added auth user prisma module
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ export default tseslint.config(
   },
   {
     rules: {
+      'prettier/prettier': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn'
